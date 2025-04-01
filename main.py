d82aef65ad53465b6a8da29289632d83d41daa15
Alijono24 <azamjonnajottalim@gmail.com>
2025-04-01 12:27:50 +0500

    Vazifa video jo'natishni topib kelish, va wifi dagi barcha foydalnuvchilar ip orqali kira olishi kk

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from wsgiref.simple_server import make_server
 
 def app(environ, start_response):
+    with open("/Users/azamjon/Desktop/java.jpg", "rb") as file:
+        data = file.read()
+
+
     status = "200 OK"
 
-    headers = [("Content-type", "text/plain")]
+    headers = [("Content-type", "image/jpg")]
 
     start_response(status, headers)
 
-    return [b"Assalomu alaykum Taqsir, 60 ta"]
+    return [data]
 
 
 server = make_server("localhost", 8000, app)
